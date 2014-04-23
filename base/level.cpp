ce87bd1d37b4db4229c630c6bfb1fb340599e7f6
mchan18 <mchan18@calpoly.edu>
2014-04-23 05:24:18 -0700

    Why won't VM run faster? :c

--- base/level.cpp
+++ base/level.cpp
@@ -3,8 +3,8 @@
 Mesh m;
 void loadLevel(){
    m = LoadMesh("../Assets/ModMBasicBldg.obj");
-   for(int strX=-36;strX<36;strX+=12){
-      for(int strZ=-36;strZ<36;strZ+=12){
+   for(int strX=-100;strX<100;strX+=45){
+      for(int strZ=-100;strZ<100;strZ+=45){
          physSetDisplayObj(createStaticBox(strX,0,strZ,1,1,1,btQuaternion(0,0,0,1),0/*INFINITE*/,0,0,0),&m); 
       }
    }
--- base/level.cpp~
+++ base/level.cpp~
@@ -2,7 +2,7 @@
 #include "Mesh.h"
 Mesh m;
 void loadLevel(){
-   m = LoadMesh("../Assets/streetlight.obj");
+   m = LoadMesh("../Assets/ModMBasicBldg.obj");
    for(int strX=-36;strX<36;strX+=12){
       for(int strZ=-36;strZ<36;strZ+=12){
          physSetDisplayObj(createStaticBox(strX,0,strZ,1,1,1,btQuaternion(0,0,0,1),0/*INFINITE*/,0,0,0),&m); 
