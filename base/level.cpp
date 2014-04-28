75c8e0739c8c2924d33dd8343e9fe837980ea796
gork852 <amcmuldr@calpoly.edu>
2014-04-28 15:59:29 -0700

    level smaller

--- base/level.cpp
+++ base/level.cpp
@@ -10,8 +10,8 @@ void loadLevel(){
    mb = LoadMesh("../Assets/ModMBasicBldg.obj");
    ms = LoadMesh("../Assets/shopBldg.obj");
    int tf = 0;
-   for(int strX=-1000;strX<1000;strX+=30){
-      for(int strZ=-1000;strZ<1000;strZ+=30){
+   for(int strX=-100;strX<100;strX+=30){
+      for(int strZ=-100;strZ<100;strZ+=30){
        //btRigidBody* createStaticBox(float posX,float posY,float posZ,float scaleX,float scaleY,float scaleZ,btQuaternion rotation,float mass,float ix,float iy,float iz);
          tf = !tf;
          if(tf){
