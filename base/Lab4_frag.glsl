f1be6b4b16227b85b88703ba86dd35a5b53e0062
Jaitnium <Jaitnium@aol.com>
2014-05-26 17:33:35 -0700

    Added spaces into frag shader

--- base/Lab4_frag.glsl
+++ base/Lab4_frag.glsl
@@ -39,15 +39,15 @@ void main() {
 
    
 
-   angleNL = clamp(dot(norm, light), 0.0, 1.0);
+      angleNL = clamp(dot(norm, light), 0.0, 1.0);
 
-   halfVec = (light + view) / length(light + view);
-   angleNH = dot(norm, halfVec);
-   angleNH = pow(clamp(angleNH, 0.0, 1.0), uMat.shine * 4.0);
+      halfVec = (light + view) / length(light + view);
+      angleNH = dot(norm, halfVec);
+      angleNH = pow(clamp(angleNH, 0.0, 1.0), uMat.shine * 4.0);
 
-   shadowPos = vShadowPos / vShadowPos.w;
-   depth = texture2D(uTexUnit, shadowPos.xy).z; // Shadow map depth = Z VALUE OF SHADOW MAP
-   dist = vShadowPos.z - 0.005; // Distance from light to fragment = FRAG DEPTH FROM LIGHT'S PERSPECTIVE
+      shadowPos = vShadowPos / vShadowPos.w;
+      depth = texture2D(uTexUnit, shadowPos.xy).z; // Shadow map depth = Z VALUE OF SHADOW MAP
+      dist = vShadowPos.z - 0.005; // Distance from light to fragment = FRAG DEPTH FROM LIGHT'S PERSPECTIVE
 
    // Diffuse lighting
 /*
@@ -58,44 +58,44 @@ void main() {
    else
       color = uLColor * uMat.dColor * 0.35;
 */
-   if (angleNL > 0.8)
-      color = uLColor * uMat.dColor;
-   else if (angleNL > 0.6)
-      color = uLColor * uMat.dColor * 0.8;
-   else if (angleNL > 0.4)
-      color = uLColor * uMat.dColor * 0.6;
-   else if (angleNL > 0.2)
-      color = uLColor * uMat.dColor * 0.4;
-   else
-      color = uLColor * uMat.dColor * 0.2;
+      if (angleNL > 0.8)
+         color = uLColor * uMat.dColor;
+      else if (angleNL > 0.6)
+         color = uLColor * uMat.dColor * 0.8;
+       else if (angleNL > 0.4)
+         color = uLColor * uMat.dColor * 0.6;
+      else if (angleNL > 0.2)
+         color = uLColor * uMat.dColor * 0.4;
+      else
+         color = uLColor * uMat.dColor * 0.2;
 
-   // Test if the fragment is in a shadow
-   if (uShadeMode == 0) {
-      if (depth < dist && dist < 1.0 && abs(angleNL) >= 0.01)
-         color *= 0.3;
-      else if (uMat.alpha == 1.0) // If not in a shadow, add specular
-         color += uLColor * uMat.sColor * angleNH;
-   }
-   else {
-      float multiplier = 0.0;
-      float offsetX, offsetY;
-
-      // Blur the shadow by averaging the surrounding shadow map values in a 3x3 texel square
-      for (offsetX = -1.0; offsetX <= 1.0; offsetX++) {
-         for (offsetY = -1.0; offsetY <= 1.0; offsetY++) {
-            // Get the shadow map depth at a neighboring texel
-            depth = texture2D(uTexUnit, vec2(shadowPos.x + offsetX / 1600.0, shadowPos.y + offsetY / 800.0)).z;
-            // If the area is not shadowed, increase the multiplier (make brighter)
-            if (!(depth < dist && dist < 1.0 && abs(angleNL) >= 0.01))
-               multiplier += 1.0;
-         }
+      // Test if the fragment is in a shadow
+      if (uShadeMode == 0) {
+        if (depth < dist && dist < 1.0 && abs(angleNL) >= 0.01)
+            color *= 0.3;
+         else if (uMat.alpha == 1.0) // If not in a shadow, add specular
+            color += uLColor * uMat.sColor * angleNH;
       }
-      // Only darken the pixel if the shadow isn't negligible. Otherwise, add specular
-      if (multiplier / 9.0 <= 0.8)
-         color *= multiplier / 9.0;
-      else if (uMat.alpha == 1.0)
-         color += uLColor * uMat.sColor * angleNH;
-   }
+      else {
+         float multiplier = 0.0;
+         float offsetX, offsetY;
+
+         // Blur the shadow by averaging the surrounding shadow map values in a 3x3 texel square
+         for (offsetX = -1.0; offsetX <= 1.0; offsetX++) {
+            for (offsetY = -1.0; offsetY <= 1.0; offsetY++) {
+               // Get the shadow map depth at a neighboring texel
+               depth = texture2D(uTexUnit, vec2(shadowPos.x + offsetX / 1600.0, shadowPos.y + offsetY / 800.0)).z;
+               // If the area is not shadowed, increase the multiplier (make brighter)
+               if (!(depth < dist && dist < 1.0 && abs(angleNL) >= 0.01))
+                  multiplier += 1.0;
+            }
+         }
+         // Only darken the pixel if the shadow isn't negligible. Otherwise, add specular
+         if (multiplier / 9.0 <= 0.8)
+            color *= multiplier / 9.0;
+         else if (uMat.alpha == 1.0)
+            color += uLColor * uMat.sColor * angleNH;
+      }
 
    // Ambient lighting
    color += uLColor * uMat.aColor;
@@ -107,5 +107,4 @@ void main() {
    //gl_FragColor = vec4(depth, depth, depth, 1.0);
    //gl_FragColor = vec4(dist, dist, dist, 1.0);
    }
-
 }
